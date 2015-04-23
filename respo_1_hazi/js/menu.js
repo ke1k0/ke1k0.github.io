5e567f72ba145c31074fd100abb0da89207c7a58
Szabó Eszter <eszterszabo@outlook.com>
2015-04-23 15:26:08 +0200

    respo 1 házi - menü js -> css

--- respo_1_hazi/js/menu.js
+++ respo_1_hazi/js/menu.js
@@ -1,5 +1,17 @@
 $(document).ready(function(){
-    $("#menu-icon").click(function(){
-        $("#menu-toggle").toggle(1000);
-    });
+    var width = window.innerWidth
+	|| document.documentElement.clientWidth
+	|| document.body.clientWidth;
+
+	$(document).ready(function(){
+	    $("#menu-toggle").hide();
+	    if (width < 977) {
+	    	$("#menu-icon").click(function(){
+		        $("#menu-toggle").toggle(300);
+		    });
+		}
+		else {
+			$("#menu-toggle").show();
+		}
+	});
 });
